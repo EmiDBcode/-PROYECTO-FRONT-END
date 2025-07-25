0cfc908d74004d9d4e21e83a6caaaf26939ebfe7
EmiDBcode <Emilianoleiva20j@gmail.com>
2025-07-25 06:18:19 -0300

    Inicio del proyecto Front end con HTML y CSS

---
+++ Javascript/Ej_1.js
@@ -0,0 +1,39 @@
+// Entonces "// " esto es un comentario 
+
+//Debemos definir una variable 
+
+/* 
+Al parecer nuestro """ """ en javascript se
+hace con lo que se ve por pantalla en  este fragmento
+y con esto podemos escribir 
+varias lineas de codigo a forma de comentario  
+*/
+
+// Entonces:
+
+//Definimos la variable
+
+//definmos la varibale
+
+// -Forma antigua-Variable de tipo global
+var edad=20;
+var apellido="Leiva"
+var meGustaJS=True;
+//
+// -Forma nueva-Variable tipo local 
+let numero=0
+let nombre="Emiliano"
+
+//Luego nuestgro print seria
+
+console.log ("Hola"+apellido);
+
+// Aunque una forma mejor seria 
+
+console.log (`Probando consola, metodo moderno. Mi edad es ${edad}`);
+
+/*
+
+
+
+*/
---
+++ Javascript/Ej_2.js
@@ -0,0 +1,7 @@
+var x = 3;
+var points = [10, 20, 30, 40, 50];
+if (points[x] == 40) {
+    console.log(points[x - 1]);
+} else {
+    console.log('Hola');
+}
